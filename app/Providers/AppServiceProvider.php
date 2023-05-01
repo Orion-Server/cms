23b7ff93978ba350c23f1d6ff8c01bd99979038f
nicollassilva <lyod.hp@gmail.com>
2023-05-01 08:21:49 -0300

    Forcing https in production env

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (App::isProduction()) {
+            URL::forceScheme('https');
+        }
     }
 }
