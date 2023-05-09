1013c1be2342a426407cae29287294e277fd2528
nicollassilva <lyod.hp@gmail.com>
2023-05-09 12:23:41 -0300

    Incorrect default isDisconnected value

--- resources/js/components/Client.js
+++ resources/js/components/Client.js
@@ -11,7 +11,7 @@ export default class Client {
             onlineCount: 0,
             showCmsFrame: false,
             onlineCountButtonDelay: false,
-            isDisconnected: true,
+            isDisconnected: false,
 
             endpoints: {
                 onlineCount: onlineCountEndpoint
