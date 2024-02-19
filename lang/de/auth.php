155f45bc838807fea7cbdc0a220adc4c1e6606a5
nicollassilva <lyod.hp@gmail.com>
2024-02-19 10:50:23 -0300

    Adding DE for translate

---
+++ lang/de/auth.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'These credentials do not match our records.',
+    'password' => 'The provided password is incorrect.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'recaptcha_failed' => 'The reCAPTCHA verification failed.',
+    'turnstile_failed' => 'The Turnstile verification failed.',
+    'max_accounts_per_ip' => 'You have reached the maximum number of accounts per IP address.',
+    'invalid_ip' => 'Invalid IP address.',
+    'registration_disabled' => 'Registration is disabled.'
+
+];
