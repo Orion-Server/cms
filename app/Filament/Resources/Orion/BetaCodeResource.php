c56edee59651024ad6dcc6d9d517b32d574dd632
nicollassilva <lyod.hp@gmail.com>
2024-06-06 18:01:49 -0300

    Fixing default sort in the BetaCodeResource

--- app/Filament/Resources/Orion/BetaCodeResource.php
+++ app/Filament/Resources/Orion/BetaCodeResource.php
@@ -49,6 +49,7 @@ class BetaCodeResource extends Resource
     public static function table(Table $table): Table
     {
         return $table
+            ->defaultSort('id', 'desc')
             ->columns([
                 TextColumn::make('id'),
 
